d1508e18e5c71d028b4f72aad367db50edf210a7
Aragas <personal@aragas.org>
2022-08-01 12:07:28 +0300

    fix

--- referenceassemblies-diff/PackageDownloader/Extensions/IAsyncEnumerableExtensions.cs
+++ referenceassemblies-diff/PackageDownloader/Extensions/IAsyncEnumerableExtensions.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using NuGet.Protocol.Core.Types;
 
 namespace PackageDownloader.Extensions
 {
@@ -7,7 +10,18 @@ namespace PackageDownloader.Extensions
     {
         public static async IAsyncEnumerable<TResult> AsAsyncEnumerable<TResult>(this Task<IEnumerable<TResult>> @this)
         {
-            foreach (var iteration in await @this)
+            IEnumerable<TResult> enumerable;
+            getEnum:
+            try
+            {
+                enumerable = await @this;
+            }
+            catch (FatalProtocolException e) when (e.InnerException is HttpRequestException { StatusCode: HttpStatusCode.InternalServerError })
+            {
+                goto getEnum;
+            }
+
+            foreach (var iteration in enumerable)
                 yield return iteration;
         }
     }
